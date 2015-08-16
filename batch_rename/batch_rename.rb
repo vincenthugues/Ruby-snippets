b3d1cc78336961caffaadde9bf2b23625e08400c
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-08-16 23:21:36 +0200

    batch_rename: clean up code

--- batch_rename/batch_rename.rb
+++ batch_rename/batch_rename.rb
@@ -1,23 +1,36 @@
-print 'Directory: '
-directory = gets.strip
-directory << '/' unless directory.end_with?('/')
+def prompt_user()
+	print 'Directory: '
+	directory = gets.strip
+	directory << '/' unless directory.end_with?('/')
 
-print 'File extension (ex: .jpg): '
-extension = gets.strip
+	print 'File extension (ex: .jpg): '
+	extension = gets.strip
 
-print 'Prefix (ex: DSC_): '
-prefix = gets.strip
-
-i = 1
-Dir[directory + '*'].each do |f|
-	next if File.directory? f
+	print 'Prefix (ex: DSC_): '
+	prefix = gets.strip
 	
-	if File.extname(File.basename(f)) == extension
-		filename = File.basename(f, extension)
-		new_filename = f.gsub(filename, prefix + i.to_s)
-		puts "Renaming " + f + " to " + new_filename
-		File.rename(new_filename)
+	return directory, extension, prefix
+end
+
+def rename_files(directory, extension, prefix)
+	n = 0
+	Dir[directory + '*'].each do |f|
+		next if File.directory? f
+		
+		if File.extname(File.basename(f)) == extension
+			filename = File.basename(f, extension)
+			new_filename = f.gsub(filename, prefix + (n+1).to_s)
+			
+			print "Renaming " + f + " to " + new_filename + "\n"
+			File.rename(f, new_filename)
+			
+			n += 1
+		end
 	end
 	
-	i += 1
+	puts "Renamed " + n.to_s + " files"
 end
+
+user_input = prompt_user
+
+rename_files *user_input
