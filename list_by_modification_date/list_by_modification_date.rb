76b08d39667c4d77515d87e56c318ab080dd9221
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-08-30 23:42:13 +0200

    Add list_by_modification_date script

---
+++ list_by_modification_date/list_by_modification_date.rb
@@ -0,0 +1,29 @@
+
+def list_files(directory)
+	if not directory.empty?
+		# Expand the path in case it points inside the user's home directory
+		directory = File.expand_path(directory)
+		# Ensure the directory ends with a slash
+		directory << '/' unless directory.end_with?('/')
+		
+		# Check that the directory exists
+		if not Dir.exists? directory
+			puts "Directory \"#{directory}\" not found"
+			return
+		end
+	end
+	
+	Dir[directory + '*'].sort_by {|f| File.mtime f }.each {|f| puts f}
+end
+
+def list_by_modification_date()
+	directory = ''
+
+	if ARGV.length == 1
+		directory = ARGV.shift.strip
+	end
+	
+	list_files directory
+end
+
+list_by_modification_date
