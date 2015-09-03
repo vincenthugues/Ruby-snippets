25a5d56fa816afc027971e2e792285e7796bb10c
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-09-03 23:58:14 +0200

    list_by_modification_date: add options handling

--- list_by_modification_date/list_by_modification_date.rb
+++ list_by_modification_date/list_by_modification_date.rb
@@ -1,5 +1,6 @@
+require 'optparse'
 
-def list_files(directory)
+def list_files(directory, options)
 	if not directory.empty?
 		# Expand the path in case it points inside the user's home directory
 		directory = File.expand_path(directory)
@@ -13,10 +14,14 @@ def list_files(directory)
 		end
 	end
 	
-	Dir[directory + '*'].sort_by { |f| File.mtime f }.each { |f| puts File.mtime(f).to_s + "\t" + f }
+	Dir[directory + '*'].sort_by { |f| File.mtime f }.each do |f|
+		s = ''
+		s += File.mtime(f).to_s + "\t"
+		puts s + f
+	end
 end
 
-def list_by_modification_date()
+def list_by_modification_date(options)
 	# Use the current directory by default
 	directory = ''
 
@@ -24,7 +29,22 @@ def list_by_modification_date()
 		directory = ARGV.shift.strip
 	end
 	
-	list_files directory
+	list_files directory, options
 end
 
-list_by_modification_date
+def get_options
+	options = {}
+	
+	OptionParser.new do |opts|
+	  opts.banner = "Usage: list_by_modification_date.rb [options]"
+
+	  opts.on("-t", "--time", "Show last modification date") do |t|
+		options[:time] = t
+	  end
+	end.parse!
+	
+	return options
+end
+
+options = get_options
+list_by_modification_date options
