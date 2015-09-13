390219ca68dafab92a8f56f6f5dbd043d9b9aaaf
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-09-13 17:42:01 +0200

    parse_git_config: move Parser class to its own file

--- parse_git_config/parse_git_config.rb
+++ parse_git_config/parse_git_config.rb
@@ -1,48 +1,12 @@
-class Parser
-    def initialize
-         @sections = []
-    end
-
-    def add_section(name, pattern)
-    	@sections << Struct.new(:name, :pattern).new(name, pattern)
-    end
-
-    def print_file(path)
-        File.open(path).each { |line| puts line }
-    end
-
-    def print_sections(path)
-    	# Create the arrays to hold the results for each section
-	sectionsResults = {}
-	@sections.each do |section|
-	    #sectionsResults << Struct.new(:name, :results).new(section.name, [])
-	    sectionsResults[section.name] = []
-	end
-
-	# Get the results from the file
-      	unless @sections.empty?
-	    File.open(path).each do |line|
-	        @sections.each do |section|
-	            line.match(section.pattern) { |result| sectionsResults[section.name] << result[1] }
-	        end
-	    end
-	end
-
-	# Display the results
-	sectionsResults.each_pair do |section_name, results|
-	    puts section_name + ':'
-	    results.each { |result| puts ' ' * 4 + result }
-	end
-    end
-end
+require_relative('parser')
 
 def main
-    parser = Parser.new
+  parser = Parser.new
 
-    parser.add_section('Remotes', /\[remote \"(.*)\"\]/)
-    parser.add_section('Branches', /\[branch \"(.*)\"\]/)
+  parser.add_section('Remotes', /\[remote \"(.*)\"\]/)
+  parser.add_section('Branches', /\[branch \"(.*)\"\]/)
 
-    parser.print_sections('../.git/config')
+  parser.print_sections('../.git/config')
 end
 
 main
