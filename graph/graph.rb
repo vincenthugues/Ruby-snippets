16a807b3dc1e8b37e00cc1324e532e964f2fb099
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-09-19 19:42:27 +0200

    graph: handle parser instance internally to Graph

--- graph/graph.rb
+++ graph/graph.rb
@@ -1,14 +1,49 @@
 require_relative('vertex')
+require_relative('parser')
 
 class Graph
   Edge = Struct.new(:src, :dest, :weight)
   
   attr_accessor :type
   
-  def initialize(graph_type=:undirected)
+  def initialize(graph_type=:undirected, filename = '')
     @type = graph_type
     @vertices = []
     @edges = []
+    
+    unless filename.empty? then build_from_file(filename) end
+  end
+  
+  def build_from_file(filename)
+    parser = Parser.new
+    
+    # Handle vertices ("vertex <Name> <XCoordinate> <YCoordinate>")
+    parser.add_section(
+      'Vertices',
+      /
+        vertex
+        \s+
+        (?<Name>\S+)
+        \s+
+        (?<XCoordinate>-?\d+)
+        \s+
+        (?<YCoordinate>-?\d+)
+      /x)
+    
+    # Handle edges ("edge <Origin> <Destination> <Weight>")
+    parser.add_section(
+      'Edges',
+      /
+        edge
+        \s+
+        (?<Origin>\S+)
+        \s+
+        (?<Destination>\S+)
+        \s+
+        (?<Weight>-?\d+)
+      /x)
+    
+    parser.print_sections(filename)
   end
   
   def add_vertex(name, x, y)
--- graph/main.rb
+++ graph/main.rb
@@ -1,23 +1,10 @@
 require_relative('graph')
-require_relative('parser')
 
 def main
-  graph = Graph.new(:undirected)
+  graph = Graph.new(graph_type = :undirected, filename = 'input/test1.txt')
   graph.add_vertex("a", 0, 0)
   graph.add_vertex("b", 1, 2)
   graph.add_edge("a", "b", 1)
-  
-  parser = Parser.new
-
-  parser.add_section(
-    'Vertices',
-    /vertex\s+(?<Name>\S+)\s+(?<XCoordinate>-?\d+)\s+(?<YCoordinate>-?\d+)/)
-  
-  parser.add_section(
-    'Edges',
-    /edge\s+(?<Origin>\S+)\s+(?<Destination>\S+)\s+(?<Weight>-?\d+)/)
-
-  parser.print_sections('input/test1.txt')
 end
 
 main
